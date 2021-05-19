e025857b5fe7da682eff4b6e611a0e94e74cf1c5
Erik <erikwaskiewicz@gmail.com>
2021-05-19 12:12:31 +0100

    Update templates

--- analysis/views.py
+++ analysis/views.py
@@ -8,75 +8,99 @@ def analysis_sheet(request):
             'lab_no': '21M12345',
         },
         'coverage_data': {
-            'BRAF': [
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-            ],
-            'KIT': [
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-            ],
-            'EGFR': [
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-                {
-                    'genomic': '3:574939',
-                    'hgvs_c': 'c.12345',
-                    'percent_135x': 10,
-                    'percent_270x': 5,
-                    'ntc': 1
-                },
-            ],
+            'BRAF': {
+                'av_coverage': 300,
+                'percent_270x': 50,
+                'percent_135x': 90,
+                'av_ntc_coverage': 1,
+                'percent_ntc': '0.2%',
+                'regions': {},
+                'gaps': [
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                ],
+            },
+            'KIT': {
+                'av_coverage': 300,
+                'percent_270x': 50,
+                'percent_135x': 90,
+                'av_ntc_coverage': 1,
+                'percent_ntc': '0.2%',
+                'regions': {},
+                'gaps': [
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                ],
+            },
+            'EGFR': {
+                'av_coverage': 300,
+                'percent_270x': 50,
+                'percent_135x': 90,
+                'av_ntc_coverage': 1,
+                'percent_ntc': '0.2%',
+                'regions': {},
+                'gaps': [
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                    {
+                        'genomic': '3:574939',
+                        'hgvs_c': 'c.12345',
+                        'percent_135x': 10,
+                        'percent_270x': 5,
+                        'ntc': 1
+                    },
+                ],
+            },
         },
         'variant_data': 'test',
         'checking_data': 'test',
