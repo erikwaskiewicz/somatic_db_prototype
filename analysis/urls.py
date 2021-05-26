5a2ab4382cbeefdd70848e6a40d42ea08765ed8a
Erik Waskiewicz <erikwaskiewicz@gmail.com>
2021-05-26 20:34:54 +0100

    Remove logout page and redirect to login instead

--- analysis/urls.py
+++ analysis/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='analysis/sign-in.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='analysis/sign-out.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='analysis/sign-in.html'), name='logout'),
 
     path('worksheets', views.view_worksheets, name='view_worksheets'),
     path('worksheets/<str:worksheet_id>', views.view_samples, name='view_samples'),
