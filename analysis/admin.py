fff3c1ce9ae0d1c50fff033ae0dbb2bfc3324381
Erik Waskiewicz <erikwaskiewicz@gmail.com>
2021-06-02 13:44:25 +0100

    Make initial models

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Run)
+admin.site.register(Worksheet)
+admin.site.register(Sample)
+admin.site.register(Panel)
+admin.site.register(SampleAnalysis)
+admin.site.register(Check)
--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+class Run(models.Model):
+    run_id = models.CharField(max_length=50, primary_key=True)
+
+
+class Worksheet(models.Model):
+    ws_id = models.CharField(max_length=50, primary_key=True)
+
+
+class Sample(models.Model):
+    sample_id = models.CharField(max_length=50, primary_key=True)
+    sample_type = models.CharField(max_length=50)  # DNA or RNA
+
+
+class Panel(models.Model):
+    """
+    """
+    panel_name = models.CharField(max_length=50, primary_key=True)
+
+
+class SampleAnalysis(models.Model):
+    """
+
+    """
+    run = models.ForeignKey('Run', on_delete=models.CASCADE)
+    worksheet = models.ForeignKey('Worksheet', on_delete=models.CASCADE)
+    sample = models.ForeignKey('Sample', on_delete=models.CASCADE)
+    panel = models.ForeignKey('Panel', on_delete=models.CASCADE)
+    
+    def get_status(self):
+        """
+        Get all associated checks and work out the status
+        """
+        checks = Check.objects.filter(analysis = self)
+        print(checks)
+        #TODO
+
+
+class Check(models.Model):
+    """
+    Model to store 1st, 2nd check etc
+
+    """
+    analysis = models.ForeignKey('SampleAnalysis', on_delete=models.CASCADE)
+    stage = models.CharField(max_length=50) # 1st check IGV, 2nd, 3rd..., interpretation
+    status = models.CharField(max_length=50)
+    user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
