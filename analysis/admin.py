670fec1b1b6d5379b473c83a590a10180e11dec7
Laura McCluskey <lauramccluskey@btinternet.com>
2021-12-07 17:44:22 +0000

    make changes to admin page to make it easier for ds

--- analysis/admin.py
+++ analysis/admin.py
@@ -4,22 +4,54 @@ from .models import *
 
 admin.site.register(Run)
 admin.site.register(Worksheet)
-admin.site.register(Sample)
-admin.site.register(Panel)
-admin.site.register(SampleAnalysis)
+
+class SampleAdmin(admin.ModelAdmin):
+    list_display = ('sample_id', 'sample_name', 'sample_type')
+admin.site.register(Sample, SampleAdmin)
+
+
+class PanelAdmin(admin.ModelAdmin):
+    list_display = ('panel_name', 'dna_or_rna')
+admin.site.register(Panel, PanelAdmin)
+
+class SampleAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'worksheet', 'sample', 'panel')
+admin.site.register(SampleAnalysis, SampleAnalysisAdmin)
 
 class CheckAdmin(admin.ModelAdmin):
     list_display = ('id', 'analysis', 'status', 'user')
 admin.site.register(Check, CheckAdmin)
 
-admin.site.register(Variant)
-admin.site.register(VariantInstance)
-admin.site.register(VariantPanelAnalysis)
-admin.site.register(VariantList)
-admin.site.register(VariantToVariantList)
-admin.site.register(VariantCheck)
+class VariantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'genomic_37', 'genomic_38')
+admin.site.register(Variant, VariantAdmin)
+
+
+class VariantInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sample', 'variant')
+admin.site.register(VariantInstance, VariantInstanceAdmin)
+
+class VariantPanelAnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sample_analysis', 'variant_instance')
+admin.site.register(VariantPanelAnalysis, VariantPanelAnalysisAdmin)
+
+class VariantListAdmin(admin.ModelAdmin):
+    list_display = ( 'name', 'list_type' )
+admin.site.register(VariantList, VariantListAdmin )
+
+
+class VariantToVariantListAdmin(admin.ModelAdmin):
+    list_display = ( 'variant_list', 'variant', 'classification')
+admin.site.register(VariantToVariantList, VariantToVariantListAdmin)
+
+
+class VariantCheckAdmin(admin.ModelAdmin):
+    list_display = ( 'id', 'variant_analysis', 'check_object', 'decision')
+admin.site.register(VariantCheck, VariantCheckAdmin)
 
 admin.site.register(Gene)
+
+
 admin.site.register(GeneCoverageAnalysis)
 admin.site.register(RegionCoverageAnalysis)
 admin.site.register(GapsAnalysis)
