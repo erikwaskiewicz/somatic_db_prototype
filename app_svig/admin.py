d176588cd5e3951e195ee4cd6fd50b2b00bd8012
Erik Waskiewicz <erikwaskiewicz@gmail.com>
2023-08-09 21:41:49 +0100

    Add code check models and code to create checks

--- app_svig/admin.py
+++ app_svig/admin.py
@@ -5,4 +5,12 @@ from .models import *
 class ClassificationAdmin(admin.ModelAdmin):
     search_fields = ['variant']
 
+class CheckAdmin(admin.ModelAdmin):
+    search_fields = ['classification']
+
+class CodeAnswerAdmin(admin.ModelAdmin):
+    search_fields = ['code', 'check_object']
+
 admin.site.register(Classification, ClassificationAdmin)
+admin.site.register(Check, CheckAdmin)
+admin.site.register(CodeAnswer, CodeAnswerAdmin)
--- app_svig/models.py
+++ app_svig/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from somatic_variant_db.settings import BASE_DIR, SVIG_CODE_VERSION
+
+import yaml
+import os
+
 
 # Create your models here.
 class Classification(models.Model):
@@ -7,37 +12,55 @@ class Classification(models.Model):
 
     """
     variant = models.ForeignKey('analysis.VariantPanelAnalysis', on_delete=models.CASCADE)
-    full_classification = models.BooleanField(default = False)
-    #svig_version = models.CharField()
-
+    full_classification = models.BooleanField(default=False)
+    svig_version = models.CharField(max_length=20)
 
     def __str__(self):
         return self.variant.variant_instance.hgvs_c
 
     def make_new_classification(self):
-        # TODO make a set of code answers here
-        print('hi')
+        """
+        make a set of code answers
+        TODO possily move to check obj as that might already be made?
+        """
+        # load in list of S-VIG codes from yaml
+        config_file = os.path.join(BASE_DIR, f'app_svig/config/svig_{SVIG_CODE_VERSION}.yaml')
+        with open(config_file) as f:
+            svig_codes = yaml.load(f, Loader=yaml.FullLoader)
 
-'''
+        # make 1st check object
+        check_obj = Check.objects.create(
+            classification = self
+        )
+
+        # loop through the codes and make code answer objects
+        for code in svig_codes:
+            CodeAnswer.objects.create(
+                code = code,
+                check_object = check_obj
+            )
+
+
 class Check(models.Model):
     """
     A check of a classification
     """
-    classification = models.ForeignKey('CodeAnswer', on_delete=models.CASCADE)
+    classification = models.ForeignKey('Classification', on_delete=models.CASCADE)
 
     def classify(self):
         print('classify')
         # TODO collect all code answers and classify
 
 
-#class CodeAnswer(models.Model):
+class CodeAnswer(models.Model):
     """
     A check of an individual code
 
     """
-    code = models.CharField()
-    check = models.ForeignKey('Check', on_delete=models.CASCADE)
+    code = models.CharField(max_length=20)
+    check_object = models.ForeignKey('Check', on_delete=models.CASCADE)
+    applied = models.BooleanField(default=False)
+    applied_strength = models.CharField(max_length=20, blank=True, null=True)
 
 
 #class Annotation(models.Model):
-'''
