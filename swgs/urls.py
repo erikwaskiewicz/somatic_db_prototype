f3fd52d839052b22e21e7f4e24ea5fc8bf95a60c
Istaisa <32853927+Istaisa@users.noreply.github.com>
2024-08-07 15:59:40 +0100

    really basic webpage for testing

--- swgs/urls.py
+++ swgs/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home_swgs, name="home_swgs"),
     path('view_runs/', views.view_runs, name="view_runs"),
-    path('view_patient_analysis/', views.view_patient_analysis, name="view_patient_analysis")
+    path('view_patient_analysis/<str:patient_id>', views.view_patient_analysis, name="view_patient_analysis")
 ]
--- swgs/views.py
+++ swgs/views.py
@@ -30,4 +30,57 @@ def view_patient_analysis(request, patient_id):
     """
     View variants in a PatientAnalysis
     """
+
+    patient_analysis = PatientAnalysis.objects.get(id=patient_id)
+
+    germline_snvs_query = GermlineVariantInstance.objects.filter(patient_analysis=patient_analysis)
+    somatic_snvs_query = SomaticVariantInstance.objects.filter(patient_analysis=patient_analysis)
+    germline_snvs = []
+    somatic_snvs = []
+
+    for v in somatic_snvs_query:
+        variant = v.variant.variant
+        gnomad = v.gnomad_popmax_af
+        if float(gnomad) >= 0.05:
+            pass
+        elif float(gnomad) == -1:
+            gnomad = "Not in Gnomad"
+            variant_dict = {
+                "pk": variant,
+                "gnomad": str(gnomad)
+            }
+            somatic_snvs.append(variant_dict)
+        else:
+            variant_dict = {
+                "pk": variant,
+                "gnomad": str(gnomad)
+            }
+            somatic_snvs.append(variant_dict)
+
+    for v in germline_snvs_query:
+        variant = v.variant.variant
+        gnomad = v.gnomad_popmax_af
+        if float(gnomad) >= 0.05:
+            print("in pass")
+            pass
+        elif float(gnomad) == -1:
+            gnomad = "Not in Gnomad"
+            variant_dict = {
+                "pk": variant,
+                "gnomad": str(gnomad)
+            }
+            germline_snvs.append(variant_dict)
+        else:
+            variant_dict = {
+                "pk": variant,
+                "gnomad": str(gnomad)
+            }
+            germline_snvs.append(variant_dict)
+    
+    context = {
+        "somatic_snvs": somatic_snvs,
+        "germline_snvs": germline_snvs
+    }
+
+    return render(request, "swgs/view_patient_analysis.html", context)
     
