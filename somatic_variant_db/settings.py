4aceccb52ad8f0fbd6a7025896eaf307bdbcee37
Erik Waskiewicz <erikwaskiewicz@gmail.com>
2025-02-12 17:53:19 +0000

    Revert DB_INSTANCE, accidentally pushed local settings

--- somatic_variant_db/settings.py
+++ somatic_variant_db/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'somatic_variant_db.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-DB_INSTANCE = 'local'
+DB_INSTANCE = 'webserver'
 
 if DB_INSTANCE == 'webserver':
     DB_PASSWORD_FILE = 'password.txt'
